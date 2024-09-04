6f2ba410db4ca9886792f04e0f70a14e151b7c55
Rahul Panchal <rm5901960@gmail.com>
2024-09-04 22:10:11 +0530

    add create_blog to app routes

---
+++ Controllers/Feed/create.controller.js
@@ -0,0 +1,26 @@
+const blogModel = require("../../Models/Feed/Blog.model")
+const userModel = require("../../Models/User/User.model")
+const create = async (req,res)=>{
+    
+    try{
+        const {title,content,author} = req.body;
+        const username = author || req.user.username
+
+        const user = await userModel.findOne({username:username })
+    
+        const blog = {
+            title: title,
+            content: content,
+            author: user._id
+        }
+        await blogModel.create(blog);
+        res.status(201).json({message:"Blog published!"})
+
+    }catch(e){
+        console.error("Error creating blog: ",e.message)
+        res.status(500).json({message: "Internal Server Error"})
+    }
+
+}
+
+module.exports = create
--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const signup = require('./Controllers/Auth/Signup.controler');
 const login = require('./Controllers/Auth/Login.controller');
+const createBlog = require('./Controllers/Feed/create.controller')
+const auth = require('./Middlewares/auth');
+const cors = require('cors')
 const app = express();
 
 app.use(express.json())
+app.use(cors())
+
 app.post('/signup',signup)
 app.post('/login',login)
+app.post('/create',auth,createBlog)
 
 
 app.get("/",(req,res)=>{
