fa00840ce946a1826affcecc405eeedfb4def9c8
Rahul Panchal <rm5901960@gmail.com>
2024-09-04 17:54:16 +0530

    define model collections

---
+++ Models/Feed/Blog.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    author:{
+        type:mongoose.Schema.ObjectId,
+        required:true,
+        ref:'Users'
+    }
+})
+
+const model = mongoose.model('Blogs',schema)
+
+module.exports = model;
