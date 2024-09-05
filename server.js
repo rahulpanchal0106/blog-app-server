9328fbc5c0a4670a1e0ed75e8797b36c170440a1
Rahul Panchal <rm5901960@gmail.com>
2024-09-06 02:28:07 +0530

    Code clean up

--- server.js
+++ server.js
@@ -4,30 +4,22 @@ const mongoose = require('mongoose')
 const start = async()=>{
     try{
         console.log("Connecting with database...")
-        await mongoose.connect(process.env.DB_CLUSTER).catch(
-            (e)=>{
-                console.error("🔴 Failed connecting to Cluster: ",e.message)
-            }
-        )
+        await mongoose.connect(process.env.DB_CLUSTER).catch((e)=>{
+            console.error("🔴 Failed connecting to Cluster: ",e.message)
+        })
+       
+        mongoose.connection.on('connected',()=>{
+            console.log("Database Cluster Connected...")
+        })
         
-        app.listen(process.env.PORT || 3000,
-            ()=>{
-                console.log("Server is listening...")
-            }
-        )
+        app.listen(process.env.PORT || 3000,()=>{
+            console.log("Server is listening...")
+        })
         
-        mongoose.connection.on('connected',
-            ()=>{
-                console.log("Database Cluster Connected...")
-            }
-        )
     }catch(e){
         console.error("Error Connecting to Database ",e.message)
     }
     
 }
-try{
-    start()
-}catch(e){
-    console.error("Error starting server: ",e.message)
-}
+
+start()
